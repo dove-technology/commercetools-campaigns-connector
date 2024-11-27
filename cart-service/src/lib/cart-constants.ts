2c44dd3220bc728e623596003528e36d93bb0c81
DownChapel <DownChapel@gmail.com>
2024-11-27 14:39:37 +0000

    Revert "Invalid formatting"
    This reverts commit d7d9c3e95409ed35d51a27931110c5c46275b2e4.

--- cart-service/src/lib/cart-constants.ts
+++ cart-service/src/lib/cart-constants.ts
@@ -2,6 +2,6 @@ const fieldPrefix = 'dovetech-discounts-';
 
 export const COUPON_CODES = `${fieldPrefix}couponCodes`;
 export const CART_METADATA = `${fieldPrefix}cartMetadata`;
-export const     CART_ACTION = `${fieldPrefix}cartAction`;
+export const CART_ACTION = `${fieldPrefix}cartAction`;
 export const EVALUATION_RESPONSE = `${fieldPrefix}evaluationResponse`;
 export const EVALUATION_CURRENCY = `${fieldPrefix}evaluationCurrency`;
