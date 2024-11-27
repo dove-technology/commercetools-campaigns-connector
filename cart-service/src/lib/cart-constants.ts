e2b2bce35c1a0237ffa3f928aff4ce76e477c5cc
DownChapel <DownChapel@gmail.com>
2024-11-27 14:43:55 +0000

    test

--- cart-service/src/lib/cart-constants.ts
+++ cart-service/src/lib/cart-constants.ts
@@ -1,6 +1,6 @@
 const fieldPrefix = 'dovetech-discounts-';
 
-export const COUPON_CODES = `${fieldPrefix}couponCodes`;
+export const    COUPON_CODES = `${fieldPrefix}couponCodes`;
 export const CART_METADATA = `${fieldPrefix}cartMetadata`;
 export const CART_ACTION = `${fieldPrefix}cartAction`;
 export const EVALUATION_RESPONSE = `${fieldPrefix}evaluationResponse`;
