d5e29a1f94ef55cb19ca5ab527d5ae67aaad94dc
Steve Hayes <steven.hayes@gmail.com>
2024-12-04 15:12:13 +0000

    logging: basic otel winston transport support

--- cart-service/src/utils/logger.utils.ts
+++ cart-service/src/utils/logger.utils.ts
@@ -1,5 +1,13 @@
 import { createApplicationLogger } from '@commercetools-backend/loggers';
 import winston from 'winston';
+import { logs } from '@opentelemetry/api-logs';
+import {
+  LoggerProvider,
+  BatchLogRecordProcessor,
+} from '@opentelemetry/sdk-logs';
+import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http';
+
+import { OpenTelemetryTransportV3 } from '@opentelemetry/winston-transport';
 
 export const logger = createApplicationLogger();
 
@@ -7,9 +15,29 @@ let loggerInstance: winston.Logger | undefined = undefined;
 
 export const getLogger = () => {
   if (!loggerInstance) {
+
+    const otlpCollectorOptions = {
+      url: `${process.env.OTEL_EXPORTER_OTLP_ENDPOINT}/v1/logs`,
+      headers: {
+        'api-key': process.env.OTEL_EXPORTER_OTLP_ENDPOINT_API_KEY!,
+      },
+      concurrencyLimit: 1,
+    };
+    
+    const loggerProvider = new LoggerProvider();
+
+    const logExporter = new OTLPLogExporter(otlpCollectorOptions);
+    loggerProvider.addLogRecordProcessor(
+      new BatchLogRecordProcessor(logExporter)
+    );
+
+    logs.setGlobalLoggerProvider(loggerProvider);
+
     loggerInstance = createApplicationLogger({
       level: process.env.LOG_LEVEL || 'info',
     });
+    
+    loggerInstance.add(new OpenTelemetryTransportV3())
   }
 
   return loggerInstance;
