107c288c0cfd3940a6e6217a24824e5ad900db07
glo <glo.pena@dovetech.com>
2024-12-06 11:01:42 +0000

    casing

--- cart-service/src/lib/cart-constants.ts
+++ cart-service/src/lib/cart-constants.ts
@@ -3,9 +3,9 @@ const fieldPrefix = 'dovetech-discounts-';
 export const EXTENSION_TYPES_DATA_LABEL = `Dovetech Extension Data`;
 export const EXTENSION_TYPES_DATA_KEY = `${fieldPrefix}extension-data`;
 
-export const COMMIT_ID = `${fieldPrefix}commitId`;
-export const COUPON_CODES = `${fieldPrefix}couponCodes`;
-export const CART_ACTION = `${fieldPrefix}cartAction`;
-export const EVALUATION_RESPONSE = `${fieldPrefix}evaluationResponse`;
-export const EVALUATION_CURRENCY = `${fieldPrefix}evaluationCurrency`;
+export const COMMIT_ID = `${fieldPrefix}commit-id`;
+export const COUPON_CODES = `${fieldPrefix}coupon-codes`;
+export const CART_ACTION = `${fieldPrefix}cart-action`;
+export const EVALUATION_RESPONSE = `${fieldPrefix}evaluation-response`;
+export const EVALUATION_CURRENCY = `${fieldPrefix}evaluation-currency`;
 export const DATA_INSTANCE = `${fieldPrefix}data-instance`;
