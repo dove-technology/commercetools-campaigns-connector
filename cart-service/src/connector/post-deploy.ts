e759b45b46c72dc182337a0d983a23a206957a8e
Steve Hayes <steven.hayes@gmail.com>
2024-12-11 09:14:02 +0000

    Added success logs to installation phases (#19)
    * added success logs to installation phases
    
    * added extra log lines for debugging
    
    * yarn format

--- cart-service/src/connector/post-deploy.ts
+++ cart-service/src/connector/post-deploy.ts
@@ -9,6 +9,8 @@ import { getLogger } from '../utils/logger.utils';
 const CONNECT_APPLICATION_URL_KEY = 'CONNECT_SERVICE_URL';
 
 async function postDeploy(properties: Map<string, unknown>): Promise<void> {
+  // todo: remove this in time
+  getLogger(false).info('In postDeploy function');
   const applicationUrl = properties.get(CONNECT_APPLICATION_URL_KEY);
 
   assertString(applicationUrl, CONNECT_APPLICATION_URL_KEY);
@@ -25,6 +27,7 @@ async function run(): Promise<void> {
     logger.info('Running post-deploy...');
     const properties = new Map(Object.entries(process.env));
     await postDeploy(properties);
+    logger.info('Successfully completed post-deploy...');
   } catch (error) {
     logger.error('Post-deploy failed:', error);
     process.exitCode = 1;
