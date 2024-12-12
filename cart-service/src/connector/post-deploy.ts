4af98d50c4c810da6da79b179ec9d56fc102ef0d
DownChapel <DownChapel@gmail.com>
2024-12-12 08:04:10 +0000

    Pass logger

--- cart-service/src/connector/post-deploy.ts
+++ cart-service/src/connector/post-deploy.ts
@@ -1,3 +1,4 @@
+import winston from 'winston';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -8,9 +9,12 @@ import { getLogger } from '../utils/logger.utils';
 
 const CONNECT_APPLICATION_URL_KEY = 'CONNECT_SERVICE_URL';
 
-async function postDeploy(properties: Map<string, unknown>): Promise<void> {
+async function postDeploy(
+  properties: Map<string, unknown>,
+  logger: winston.Logger
+): Promise<void> {
   // todo: remove this in time
-  getLogger(false).info('In postDeploy function');
+  logger.info('In postDeploy function');
   const applicationUrl = properties.get(CONNECT_APPLICATION_URL_KEY);
 
   assertString(applicationUrl, CONNECT_APPLICATION_URL_KEY);
@@ -23,15 +27,20 @@ async function postDeploy(properties: Map<string, unknown>): Promise<void> {
 async function run(): Promise<void> {
   const logger = getLogger(false);
 
+  // logger.on('finish', function () {
+  //   process.exit();
+  // });
+
   try {
     logger.info('Running post-deploy...');
     const properties = new Map(Object.entries(process.env));
-    await postDeploy(properties);
+    await postDeploy(properties, logger);
     logger.info('Successfully completed post-deploy...');
-    logger.end();
   } catch (error) {
     logger.error('Post-deploy failed:', error);
     process.exitCode = 1;
+  } finally {
+    // logger.end();
   }
 }
 
