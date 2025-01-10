a7a9f4f7f59c59a998be7f002b465d0289c8bc4b
DownChapel <DownChapel@gmail.com>
2025-01-10 09:43:12 +0000

    Mock config

--- cart-service/src/connector/post-deploy.test.ts
+++ cart-service/src/connector/post-deploy.test.ts
@@ -3,6 +3,7 @@ import { run } from './post-deploy';
 import { createCartUpdateExtension, createCustomTypes } from './actions';
 
 jest.mock('./actions');
+jest.mock('../utils/config.utils');
 
 it('post-deploy calls create extension and custom types', async () => {
   await run();
--- cart-service/src/connector/pre-undeploy.test.ts
+++ cart-service/src/connector/pre-undeploy.test.ts
@@ -3,6 +3,7 @@ import { run } from './pre-undeploy';
 import { deleteCartUpdateExtension } from './actions';
 
 jest.mock('./actions');
+jest.mock('../utils/config.utils');
 
 it('pre-undeploy calls delete extension', async () => {
   await run();
